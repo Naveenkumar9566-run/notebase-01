f3deb686f62a0db7586718b11f96d0ea8ae3a618
Naveenkumar9566-run <naveenreigns9966@gmail.com>
2025-11-08 15:29:39 +0530

    10:payments

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,10 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@/lib/db";
+import { checkout, polar, portal } from "@polar-sh/better-auth";
+import { polarClient } from "./polar";
+
+
 
 export const auth = betterAuth({
  database: prismaAdapter(prisma, {
@@ -10,4 +14,24 @@ export const auth = betterAuth({
   enabled: true,
   autoSignIn: true,
  },
+ plugins: [
+  polar({
+    client: polarClient,
+    createCustomerOnSignUp: true,
+    use: [
+      checkout({
+         products: [
+          {
+            productId: "40924ea4-959d-4e87-a7ec-f0338468b296",
+            slug: "Pro",
+          }
+        ],
+        successUrl: process.env.POLAR_SUCCESS_URL,
+        authenticatedUsersOnly: true,
+      }),
+      portal(),
+    ],
+  })
+ ]
 });
+
