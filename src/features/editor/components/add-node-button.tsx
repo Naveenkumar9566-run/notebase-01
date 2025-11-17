3ba0380a2ada3d1a4866a8694dce19bb40b36bfa
Naveenkumar9566-run <naveenreigns9966@gmail.com>
2025-11-17 17:38:03 +0530

    17: editor state

--- src/features/editor/components/add-node-button.tsx
+++ src/features/editor/components/add-node-button.tsx
@@ -11,7 +11,7 @@ export const AddNodeButton = memo (() => {
   return (
     <NodeSelector open={selectorOpen} onOpenChange={setSelectionOpen}>
     <Button
-    onClick={() => {}}
+    onClick={() => setSelectionOpen(true)}
     size="icon"
     variant="outline"
     className="bg-background"
